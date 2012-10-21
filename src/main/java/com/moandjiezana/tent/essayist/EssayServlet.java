9a8c064b1a822129152b0dcf4203946967600a79
moandji.ezana <mwanji@gmail.com>
2012-10-21 02:21:39 +0200

    Author can delete an essay

--- src/main/java/com/moandjiezana/tent/essayist/EssayServlet.java
+++ src/main/java/com/moandjiezana/tent/essayist/EssayServlet.java
@@ -1,5 +1,6 @@
 package com.moandjiezana.tent.essayist;
 
+import com.google.common.base.Throwables;
 import com.moandjiezana.tent.client.TentClient;
 import com.moandjiezana.tent.client.posts.Post;
 import com.moandjiezana.tent.client.users.Profile;
@@ -8,6 +9,7 @@ import com.moandjiezana.tent.essayist.tent.Entities;
 import java.io.IOException;
 
 import javax.inject.Inject;
+import javax.inject.Provider;
 import javax.inject.Singleton;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -19,10 +21,12 @@ public class EssayServlet extends HttpServlet {
   
   private Templates templates;
   private Users users;
+  private Provider<EssayistSession> sessions;
 
   @Inject
-  public EssayServlet(Users users, Templates templates) {
+  public EssayServlet(Users users, Provider<EssayistSession> sessions, Templates templates) {
     this.users = users;
+    this.sessions = sessions;
     this.templates = templates;
   }
 
@@ -48,5 +52,31 @@ public class EssayServlet extends HttpServlet {
     Post post = authorTentClient.getPost(essayId);
     
     templates.essay().render(resp.getWriter(), post, user.getProfile());
+  }
+  
+  @Override
+  protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    String[] parts = req.getPathInfo().split("/essay/");
+    String authorEntity = parts[0];
+    String fullAuthorEntity = Entities.expandFromUrl(authorEntity);
+    
+    User user = sessions.get().getUser();
+    
+    if (!fullAuthorEntity.equals(user.getProfile().getCore().getEntity())) {
+      resp.sendError(HttpServletResponse.SC_FORBIDDEN, "You are not permitted to delete this Essay.");
+      return;
+    }
+    
+    TentClient tentClient = new TentClient(user.getProfile());
+    tentClient.getAsync().setAccessToken(user.getAccessToken());
+    tentClient.getAsync().setRegistrationResponse(user.getRegistration());
+
+    try {
+      tentClient.getAsync().deletePost(parts[1]).get();
+    } catch (Exception e) {
+      Throwables.propagate(Throwables.getRootCause(e));
+    }
+    
+    resp.sendRedirect(req.getContextPath() + "/" + authorEntity + "/essays");
   }
 }
