aa8b71c8bc55e582263775fe5e8aba087af49204
moandji.ezana <mwanji@gmail.com>
2013-01-16 22:12:41 +0200

    Using WebJars and Servlet 3

--- src/main/java/com/moandjiezana/tent/essayist/config/Routes.java
+++ src/main/java/com/moandjiezana/tent/essayist/config/Routes.java
@@ -11,6 +11,8 @@ import java.util.List;
 import javax.inject.Inject;
 import javax.servlet.http.HttpServletRequest;
 
+import org.webjars.AssetLocator;
+
 @RequestScoped
 public class Routes {
 
@@ -20,33 +22,45 @@ public class Routes {
   public Routes(HttpServletRequest req) {
     this.req = req;
   }
-  
-  public String asset(String asset) {
-    String url = req.getContextPath() + "/assets/" + asset;
-    
+
+  public String webJar(String asset) {
+    String url = req.getContextPath() + "/" + AssetLocator.getWebJarPath(asset);
+
     if (asset.endsWith(".css")) {
       return "<link href=\"" + url + "\"  rel=\"stylesheet\" type=\"text/css\" >";
     } else if (asset.endsWith(".js")) {
       return "<script src=\"" + url + "\" type=\"text/javascript\"></script>";
     }
-    
+
     throw new IllegalArgumentException("Unknown asset type: " + asset);
   }
-  
+
+  public String asset(String asset) {
+    String url = req.getContextPath() + "/assets/" + asset;
+
+    if (asset.endsWith(".css")) {
+      return "<link href=\"" + url + "\"  rel=\"stylesheet\" type=\"text/css\" >";
+    } else if (asset.endsWith(".js")) {
+      return "<script src=\"" + url + "\" type=\"text/javascript\"></script>";
+    }
+
+    throw new IllegalArgumentException("Unknown asset type: " + asset);
+  }
+
   public String assets(String name, String asset, String... assets) {
     List<String> tags = new ArrayList<String>(assets.length);
     tags.add(asset(asset));
     for (String asset2 : assets) {
       tags.add(asset(asset2));
     }
-    
+
     return Joiner.on("\n").join(tags);
   }
-  
+
   public String essay(Post essay) {
     return req.getContextPath() + "/" + Entities.getForUrl(essay.getEntity()) + "/essay/" + essay.getId();
   }
-  
+
   public String comment(Post essay) {
     return essay(essay) + "/status";
   }
