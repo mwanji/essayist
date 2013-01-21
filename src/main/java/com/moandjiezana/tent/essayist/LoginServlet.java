8811cc19bdfbe62c64a0daf235fdcd668f92e2f8
moandji.ezana <mwanji@gmail.com>
2013-01-21 22:44:02 +0200

    Handle revoked authorization

--- src/main/java/com/moandjiezana/tent/essayist/LoginServlet.java
+++ src/main/java/com/moandjiezana/tent/essayist/LoginServlet.java
@@ -4,6 +4,7 @@ import com.moandjiezana.essayist.posts.Bookmark;
 import com.moandjiezana.essayist.posts.EssayistMetadataContent;
 import com.moandjiezana.essayist.posts.Favorite;
 import com.moandjiezana.tent.client.TentClient;
+import com.moandjiezana.tent.client.apps.App;
 import com.moandjiezana.tent.client.apps.AuthorizationRequest;
 import com.moandjiezana.tent.client.apps.RegistrationRequest;
 import com.moandjiezana.tent.client.apps.RegistrationResponse;
@@ -27,7 +28,7 @@ import javax.servlet.http.HttpServletResponse;
 
 @Singleton
 public class LoginServlet extends HttpServlet {
-  
+
   private final Users users;
   private final Templates templates;
   private final Essays essays;
@@ -40,12 +41,13 @@ public class LoginServlet extends HttpServlet {
     this.sessions = sessions;
     this.templates = jamonContext;
   }
-  
+
+  @Override
   protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException ,IOException {
     if (req.getSession(false) != null) {
       User user = (User) req.getSession().getAttribute(User.class.getName());
       EssayistSession session = sessions.get();
-      
+
       if (!session.isLoggedIn()) {
         AuthResult authResult = (AuthResult) req.getSession().getAttribute(req.getParameter("state"));
         if (authResult != null) {
@@ -55,25 +57,25 @@ public class LoginServlet extends HttpServlet {
         } else if ("server_error".equals(req.getParameter("error"))) {
           String entity = (String) req.getSession().getAttribute("entity");
           users.delete(entity);
-          
+
           TentClient tentClient = new TentClient(entity);
           RegistrationResponse registrationResponse = register(tentClient, req);
           String redirectUri = registrationResponse.getRedirectUris()[0];
           String authorizationUrl = authorize(tentClient, registrationResponse, redirectUri, req);
-          
+
           resp.sendRedirect(authorizationUrl);
 
           return;
         }
       }
-      
+
       if (user != null) {
         resp.sendRedirect(req.getContextPath() + "/read");
-        
+
         return;
       }
     }
-    
+
     templates.login().render(resp.getWriter());
   };
 
@@ -84,17 +86,27 @@ public class LoginServlet extends HttpServlet {
       entity = entity.substring(0, entity.length() - 1);
     }
     User user = users.getByEntityOrNull(entity);
-    
+
     TentClient tentClient;
     RegistrationResponse registrationResponse;
     String redirectUri;
-    
+
     if (user != null && user.getRegistration() != null) {
       tentClient = new TentClient(user.getProfile());
       tentClient.getAsync().setAccessToken(user.getAccessToken());
       tentClient.getAsync().setRegistrationResponse(user.getRegistration());
-      redirectUri = user.getRegistration().getRedirectUris()[1];
-      registrationResponse = user.getRegistration();
+
+      App app = tentClient.getApp();
+
+      if (app != null && app.getAuthorizations().length > 0) {
+        redirectUri = user.getRegistration().getRedirectUris()[1];
+        registrationResponse = user.getRegistration();
+      } else {
+        tentClient = new TentClient(entity);
+        registrationResponse = register(tentClient, req);
+        redirectUri = registrationResponse.getRedirectUris()[0];
+      }
+
     } else {
       tentClient = new TentClient(entity);
       registrationResponse = register(tentClient, req);
@@ -102,40 +114,40 @@ public class LoginServlet extends HttpServlet {
     }
 
     String authorizationUrl = authorize(tentClient, registrationResponse, redirectUri, req);
-    
+
     resp.sendRedirect(authorizationUrl);
   }
-  
+
   private RegistrationResponse register(TentClient tentClient, HttpServletRequest req) throws MalformedURLException {
     tentClient.getProfile();
-    
+
     Map<String, String> scopes = new HashMap<String, String>();
     scopes.put("write_posts", "Allows you to write Essays and re-post, bookmark or favorite other people's Essays.");
     scopes.put("read_posts", "Read Essays and your reactions to Essays.");
-    
+
     URL url = new URL(req.getRequestURL().toString());
     String baseUrl = url.getProtocol() + "://" + url.getAuthority() + req.getContextPath();
     String afterAuthorizationUrl = baseUrl + "/accessToken";
     String afterLoginUrl = baseUrl;
-    
+
     RegistrationRequest registrationRequest = new RegistrationRequest("Essayist", "A blogging app for when you need more than 256 characters.", baseUrl, new String [] { afterAuthorizationUrl, afterLoginUrl }, scopes);
-    
+
     return tentClient.register(registrationRequest);
   }
-  
+
   private String authorize(TentClient tentClient, RegistrationResponse registrationResponse, String redirectUri, HttpServletRequest req) {
     AuthorizationRequest authorizationRequest = new AuthorizationRequest(registrationResponse.getMacKeyId(), redirectUri);
     authorizationRequest.setScope("write_posts", "read_posts");
     authorizationRequest.setTentPostTypes(Post.Types.essay("v0.1.0"), Post.Types.status("v0.1.0"), Post.Types.photo("v0.1.0"), Post.Types.repost("v0.1.0"), EssayistMetadataContent.URI, Bookmark.URI, Favorite.URI);
     authorizationRequest.setState(UUID.randomUUID().toString());
     String authorizationUrl = tentClient.buildAuthorizationUrl(authorizationRequest);
-    
+
     AuthResult authResult = new AuthResult();
     authResult.profile = tentClient.getProfile();
     authResult.registrationResponse = registrationResponse;
     req.getSession().setAttribute(authorizationRequest.getState(), authResult);
     req.getSession().setAttribute("entity", tentClient.getProfile().getCore().getEntity());
-    
+
     return authorizationUrl;
   }
 }
