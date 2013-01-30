d2b1e62811c9b2c7c549c15c25c5da7d125289a5
moandji.ezana <mwanji@gmail.com>
2013-01-30 21:25:21 +0200

    Created separate migration for domain column

--- src/main/java/com/moandjiezana/essayist/db/migrations/Migration_1.java
+++ src/main/java/com/moandjiezana/essayist/db/migrations/Migration_1.java
@@ -5,20 +5,21 @@ import static com.eroi.migrate.Define.column;
 import static com.eroi.migrate.Define.notnull;
 import static com.eroi.migrate.Define.primarykey;
 import static com.eroi.migrate.Define.table;
+import static com.eroi.migrate.Define.DataTypes.BIGINT;
 import static com.eroi.migrate.Define.DataTypes.LONGVARCHAR;
 import static com.eroi.migrate.Define.DataTypes.VARCHAR;
 import static com.eroi.migrate.Execute.createTable;
+import static com.eroi.migrate.Execute.dropTable;
 
 import com.eroi.migrate.Define;
-import com.eroi.migrate.Execute;
 import com.eroi.migrate.Migration;
 
 public class Migration_1 implements Migration {
-  
+
   @Override
   public void up() {
-    createTable(table("AUTHORIZATIONS", 
-        column("ID", Define.DataTypes.BIGINT, primarykey(), autoincrement(), notnull()),
+    createTable(table("AUTHORIZATIONS",
+        column("ID", BIGINT, primarykey(), autoincrement(), notnull()),
         column("ENTITY", VARCHAR, notnull(), Define.length(1000)),
         column("PROFILE", LONGVARCHAR),
         column("REGISTRATION", LONGVARCHAR),
@@ -27,7 +28,6 @@ public class Migration_1 implements Migration {
 
   @Override
   public void down() {
-    Execute.dropTable("authorizations");
+    dropTable("authorizations");
   }
-
 }
