6789d428a34a9736cdc15e5b6fcb5f84e1b334c1
moandji.ezana <mwanji@gmail.com>
2013-01-30 23:47:11 +0200

    Using static imports

--- src/main/java/com/moandjiezana/tent/essayist/db/migrations/Migration_2.java
+++ src/main/java/com/moandjiezana/tent/essayist/db/migrations/Migration_2.java
@@ -1,24 +1,24 @@
 package com.moandjiezana.tent.essayist.db.migrations;
 
+import static com.eroi.migrate.Define.column;
+import static com.eroi.migrate.Define.defaultValue;
 import static com.eroi.migrate.Define.length;
 import static com.eroi.migrate.Define.notnull;
 import static com.eroi.migrate.Define.DataTypes.VARCHAR;
 import static com.eroi.migrate.Execute.addColumn;
 import static com.eroi.migrate.Execute.dropColumn;
 
-import com.eroi.migrate.Define;
 import com.eroi.migrate.Migration;
 
 public class Migration_2 implements Migration {
 
   @Override
   public void up() {
-    addColumn(Define.column("DOMAIN", VARCHAR, notnull(), length(1000), Define.defaultValue("")), "AUTHORIZATIONS");
+    addColumn(column("DOMAIN", VARCHAR, notnull(), length(1000), defaultValue("")), "AUTHORIZATIONS");
   }
 
   @Override
   public void down() {
     dropColumn("DOMAIN", "AUTHORIZATIONS");
   }
-
 }
