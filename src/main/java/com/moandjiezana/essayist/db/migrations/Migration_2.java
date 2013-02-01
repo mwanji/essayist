ee8dea300a3d1217ebbab9d78f5ce8cb2a3d043d
moandji.ezana <mwanji@gmail.com>
2013-02-01 14:01:57 +0200

    User#domain can be null

--- src/main/java/com/moandjiezana/essayist/db/migrations/Migration_2.java
+++ src/main/java/com/moandjiezana/essayist/db/migrations/Migration_2.java
@@ -1,19 +1,18 @@
 package com.moandjiezana.essayist.db.migrations;
 
+import static com.eroi.migrate.Define.column;
 import static com.eroi.migrate.Define.length;
-import static com.eroi.migrate.Define.notnull;
 import static com.eroi.migrate.Define.DataTypes.VARCHAR;
 import static com.eroi.migrate.Execute.addColumn;
 import static com.eroi.migrate.Execute.dropColumn;
 
-import com.eroi.migrate.Define;
 import com.eroi.migrate.Migration;
 
 public class Migration_2 implements Migration {
 
   @Override
   public void up() {
-    addColumn(Define.column("DOMAIN", VARCHAR, notnull(), length(1000), Define.defaultValue("")), "AUTHORIZATIONS");
+    addColumn(column("DOMAIN", VARCHAR, length(1000)), "AUTHORIZATIONS");
   }
 
   @Override
